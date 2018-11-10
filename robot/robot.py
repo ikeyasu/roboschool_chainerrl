91f5a9752791d2cf8565dec54f6a18964fb62dc9
Yasuki Ikeuchi <ikeyasu@gmail.com>
2018-11-11 00:27:06 +0900

    fix degreeπ

--- robot/robot.py
+++ robot/robot.py
@@ -21,11 +21,11 @@ CHANNEL0_ADDRESS_INTERVAL = 4
 # See https://learn.sparkfun.com/tutorials/pi-servo-hat-hookup-guide#software---python
 # and http://akizukidenshi.com/download/ds/towerpro/SG90_a.pdf
 ROTATION_STEP = 5000.0 / 4095.0
-DEGREE_0 = int(1.45 * 1000 / ROTATION_STEP)  # 1.45 ms
-DEGREE_n90 = int(0.5 * 1000 / ROTATION_STEP)  # 0.5ms
-DEGREE_90 = int(2.4 * 1000 / ROTATION_STEP)  # 2.4ms
-DEGREE_STEP_MIN = 0.5
-DEGREE_STEP = (1.45 - DEGREE_STEP_MIN) / 90.0
+DEGREE_0 = int(1.45 * 1000 / ROTATION_STEP)  # 1.45 ms = 1187
+DEGREE_n90 = int(0.5 * 1000 / ROTATION_STEP)  # 0.5ms = 409
+DEGREE_90 = int(2.4 * 1000 / ROTATION_STEP)  # 2.4ms = 1965
+DEGREE_PWM_0 = 1.45
+DEGREE_PWM_STEP = (1.45 - 0.5) / 90.0
 
 # Etc
 SERVO_COUNT = 8
@@ -35,6 +35,7 @@ DEBUG = True
 def _dp(msg):
     if DEBUG:
         print(msg)
+
 
 # noinspection PyMethodMayBeStatic
 class ServoDebug:
@@ -48,10 +49,14 @@ class ServoDebug:
 
 class Servo:
 
-    def __init__(self, servo_count: int):
+    def __init__(self, servo_count: int, bus=None):
         _dp("Servo init:servo_count={}".format(servo_count))
-        import smbus
-        self.bus = smbus.SMBus(1)  # the chip is on bus 1 of the available I2C buses
+        if bus is None:
+            import smbus
+            # the chip is on bus 1 of the available I2C buses
+            self.bus = smbus.SMBus(1)
+        else:
+            self.bus = bus
         self.bus.write_byte_data(CHIP_ADDRESS, 0, 0x20)  # enable the chip
         self.bus.write_byte_data(CHIP_ADDRESS, 0xfe, 0x1e)  # configure the chip for multi-byte write
         for i in range(0, servo_count):
@@ -68,12 +73,13 @@ class Servo:
     def rotate(self, channel: int, degree: float):
         start_address = CHANNEL0_START_ADDRESS + CHANNEL0_ADDRESS_INTERVAL * channel
         end_address = CHANNEL0_END_ADDRESS + CHANNEL0_ADDRESS_INTERVAL * channel
-        degree_pwm = int((float(degree) * DEGREE_STEP + DEGREE_STEP_MIN) * 1000) + DEGREE_0
+        degree_pwm = int((DEGREE_PWM_0 + float(degree) * DEGREE_PWM_STEP) * 1000 / ROTATION_STEP)
 
         self.bus.write_word_data(CHIP_ADDRESS, start_address, 0)
         self.bus.write_word_data(CHIP_ADDRESS, end_address, degree_pwm)
         _dp("Servo rotate: channel={}, degree={}, start_address={}, end_address={}, degree_pwm={}"
             .format(channel, degree, start_address, end_address, degree_pwm))
+
 
 def _get(address, port):
     url = "http://{}:{}".format(address, port)
