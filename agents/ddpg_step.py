6218e8e3c3adc815b30ef1e8615ccd1bbcb9592c
ikeyasu <ikeyasu@gmail.com>
2019-02-19 14:57:13 +0900

    Revert part of refactoring

--- agents/ddpg_step.py
+++ agents/ddpg_step.py
@@ -6,9 +6,25 @@ from chainerrl.agents import DDPG
 
 
 class DDPGStep(DDPG):
-    def __init__(self,
-                 skip_step: int = 0, **kwargs):
-        DDPG.__init__(self, **kwargs)
+    def __init__(self, model, actor_optimizer, critic_optimizer, replay_buffer,
+                 gamma, explorer,
+                 gpu=None, replay_start_size=50000,
+                 minibatch_size=32, update_interval=1,
+                 target_update_interval=10000,
+                 phi= lambda x: x,
+                 target_update_method='hard',
+                 soft_update_tau=1e-2,
+                 n_times_update=1, average_q_decay=0.999,
+                 average_loss_decay=0.99,
+                 episodic_update=False,
+                 episodic_update_len=None,
+                 skip_step: int = 25):
+        DDPG.__init__(self, model, actor_optimizer, critic_optimizer, replay_buffer,
+                      gamma, explorer, gpu, replay_start_size, minibatch_size,
+                      update_interval, target_update_interval,
+                      phi, target_update_method, soft_update_tau, n_times_update,
+                      average_q_decay, average_loss_decay,
+                      episodic_update, episodic_update_len)
         self.skip_step = skip_step
 
     def act_and_train(self, obs, reward):
