bfa0c61fa58f6af3fbeda4996b9a8ae5e540eaef
ikeyasu <ikeyasu@gmail.com>
2019-02-19 14:31:33 +0900

    Add max-runtime for launching sagemaker training instance

--- sagemaker/start_train.py
+++ sagemaker/start_train.py
@@ -9,6 +9,7 @@ from sagemaker.chainer.estimator import Chainer
 IMAGE = '398271760466.dkr.ecr.ap-northeast-1.amazonaws.com/roboschool_chainerrl:cpu'
 DEFAULT_INSTANCE_TYPE = 'ml.m4.4xlarge'
 DEFAULT_REGION = 'ap-northeast-1'
+DEFAULT_RUNTIME = 24 * 60 * 60  # 1day
 
 
 def main(parser=argparse.ArgumentParser()):
@@ -20,6 +21,7 @@ def main(parser=argparse.ArgumentParser()):
     parser.add_argument('--instance-type', type=str, default=DEFAULT_INSTANCE_TYPE)
     parser.add_argument('--region', type=str, default=DEFAULT_REGION)
     parser.add_argument('--arn', type=str, default=None)
+    parser.add_argument('--max-runtime', type=int, default=DEFAULT_RUNTIME, help='seconds')
     args = parser.parse_args()
 
     boto_session = Session(profile_name=args.profile, region_name=args.region)
@@ -43,7 +45,8 @@ def main(parser=argparse.ArgumentParser()):
                                 train_instance_count=1,
                                 train_instance_type='local' if args.local_mode else args.instance_type,
                                 hyperparameters=hyperparameters,
-                                base_job_name='roboschool-chainerrl')
+                                base_job_name='roboschool-chainerrl',
+                                train_max_run=args.max_runtime)
     chainer_estimator.fit(wait=args.local_mode)
 
 
