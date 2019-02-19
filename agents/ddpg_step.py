1fc45f83178a79a75fe6a54a66ca7cd0a5b90d02
ikeyasu <ikeyasu@gmail.com>
2019-02-19 14:47:54 +0900

    Refactoring DDPGStep

--- agents/ddpg_step.py
+++ agents/ddpg_step.py
@@ -2,29 +2,13 @@
 Copyright 2018 ikeyasu.com
 LICENSE: MIT License
 """
-import chainer
-from chainer import cuda
 from chainerrl.agents import DDPG
 
 
 class DDPGStep(DDPG):
-    def __init__(self, model, actor_optimizer, critic_optimizer, replay_buffer,
-                 gamma, explorer,
-                 gpu=None, replay_start_size=50000,
-                 minibatch_size=32, update_interval=1,
-                 target_update_interval=10000,
-                 phi=lambda x: x,
-                 target_update_method='hard',
-                 soft_update_tau=1e-2,
-                 n_times_update=1, average_q_decay=0.999,
-                 average_loss_decay=0.99,
-                 episodic_update=False,
-                 episodic_update_len=None,
-                 skip_step: int = 25):
-        DDPG.__init__(self, model, actor_optimizer, critic_optimizer, replay_buffer,
-                      gamma, explorer, gpu, replay_start_size, minibatch_size, update_interval, target_update_interval,
-                      phi, target_update_method, soft_update_tau, n_times_update, average_q_decay, average_loss_decay,
-                      episodic_update, episodic_update_len)
+    def __init__(self,
+                 skip_step: int = 0, **kwargs):
+        DDPG.__init__(self, **kwargs)
         self.skip_step = skip_step
 
     def act_and_train(self, obs, reward):
@@ -33,7 +17,7 @@ class DDPGStep(DDPG):
 
         greedy_action = self.act(obs)
 
-        if self.t % self.skip_step != 0:
+        if self.skip_step > 0 and self.last_action is not None and self.t % self.skip_step != 0:
             action = self.last_action
         else:
             action = self.explorer.select_action(self.t, lambda: greedy_action)
@@ -60,4 +44,4 @@ class DDPGStep(DDPG):
 
         self.replay_updater.update_if_necessary(self.t)
 
-        return self.last_action
+        return action
